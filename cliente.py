99838a817697052b52baaddfffd5ded082983351
J. Miguel Aguario Diaz <i20213tn095@utez.edu.mx>
2023-11-28 00:24:05 -0600

    [FIX] Cifrar

--- cliente.py
+++ cliente.py
@@ -1,31 +1,34 @@
-# cliente.py
-from functions import cifrar
 import socket
 
+def cifrar_mensaje(mensaje):
+    cifrado = ""
+    for char in mensaje:
+        if char.isalpha():
+            cifrado += chr((ord(char) - 65 + 3) % 26 + 65) if char.isupper() else chr((ord(char) - 97 + 3) % 26 + 97)
+        else:
+            cifrado += char
+    return cifrado
+
 def main():
-  # Configura el cliente
-  host = '192.168.100.117'
-  port = 5555
+    # Configura el cliente
+    host = '192.168.100.117'
+    port = 5555
 
-  # Crea un socket
-  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Crea un socket
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-  # Conéctate al servidor
-  client.connect((host, port))
+    # Conéctate al servidor
+    client.connect((host, port))
 
-  # Pide al usuario que ingrese una clave de cifrado
-  key = input("Ingrese la clave de cifrado: ")
-  while True:
-    #mandar la clave al servidor
-    client.send(key.encode('utf-8'))
-    # Lee el mensaje del usuario
-    message = input("Yo: ")
+    while True:
+        # Lee el mensaje del usuario
+        message = input("Yo: ")
 
-    # Cifra el mensaje
-    ciphertext = cifrar(message, int(key))
+        # Cifra el mensaje antes de enviarlo
+        mensaje_cifrado = cifrar_mensaje(message)
 
-    # Envía el mensaje cifrado al servidor
-    client.send(ciphertext.encode('utf-8'))
+        # Envía el mensaje cifrado al servidor
+        client.send(mensaje_cifrado.encode('utf-8'))
 
 if __name__ == "__main__":
-  main()
+    main()
--- server.py
+++ server.py
@@ -1,52 +1,49 @@
-# server.py
-
 import socket
 import threading
-import functions as cifrado
+
+def cifrar_mensaje(mensaje):
+    cifrado = ""
+    for char in mensaje:
+        if char.isalpha():
+            cifrado += chr((ord(char) - 65 + 3) % 26 + 65) if char.isupper() else chr((ord(char) - 97 + 3) % 26 + 97)
+        else:
+            cifrado += char
+    return cifrado
 
 def handle_client(client_socket, address):
-  while True:
-    # Espera a recibir datos del cliente
-    data = client_socket.recv(1024).decode('utf-8')
-    # recibe la clave de cifrado
-    key = int(data)
-    if not data:
-      break
+    while True:
+        # Espera a recibir datos del cliente
+        data = client_socket.recv(1024).decode('utf-8')
+        if not data:
+            break
 
-    # Cifra el mensaje
-    ciphertext = cifrado.cifrar(data, int(key))
+        # Descifra el mensaje recibido
+        mensaje_descifrado = cifrar_mensaje(data)
 
-    # Imprime el mensaje cifrado
-    print(f"Mensaje de {address[0]}:{address[1]}: {ciphertext}")
+        print(f"Mensaje de {address[0]}:{address[1]}: {mensaje_descifrado}")
 
-    # Descifra el mensaje cifrado
-    plaintext = cifrado.descifrar(ciphertext, int(key))
-
-    # Imprime el mensaje descifrado
-    print(f"Mensaje descifrado: {plaintext}")
-
-  client_socket.close()
+    client_socket.close()
 
 def main():
-  # Configura el servidor
-  host = '0.0.0.0'
-  port = 5555
+    # Configura el servidor
+    host = '0.0.0.0'
+    port = 5555
 
-  # Crea un socket
-  server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  server.bind((host, port))
-  server.listen(5)
+    # Crea un socket
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(5)
 
-  print(f"[*] Servidor escuchando en {host}:{port}")
+    print(f"[*] Servidor escuchando en {host}:{port}")
 
-  while True:
-    # Acepta conexiones entrantes
-    client, address = server.accept()
-    print(f"[*] Conexión aceptada de {address[0]}:{address[1]}")
+    while True:
+        # Acepta conexiones entrantes
+        client, address = server.accept()
+        print(f"[*] Conexión aceptada de {address[0]}:{address[1]}")
 
-    # Inicia un hilo para manejar la conexión del cliente
-    client_handler = threading.Thread(target=handle_client, args=(client, address))
-    client_handler.start()
+        # Inicia un hilo para manejar la conexión del cliente
+        client_handler = threading.Thread(target=handle_client, args=(client, address))
+        client_handler.start()
 
 if __name__ == "__main__":
-  main()
+    main()
