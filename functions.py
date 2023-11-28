79e1f8f5221ddf5386f524766d552f7d0d6f086c
J. Miguel Aguario Diaz <i20213tn095@utez.edu.mx>
2023-11-28 00:07:19 -0600

    [FIX] Cifrar

--- functions.py
+++ functions.py
@@ -1,18 +1,24 @@
-def cifrar(message, key):
-  """Cifra un mensaje usando el método Cesar.
-
-  Args:
-    message: El mensaje a cifrar.
-    key: La clave de cifrado.
-
-  Returns:
-    El mensaje cifrado.
-  """
-
+""" def cifrar(message, key):
   ciphertext = ""
   for char in message:
     ciphertext += chr((ord(char) + key) % 256)
+  return ciphertext """
+
+def cifrar(message, key):
+  ciphertext = ""
+  for char in message:
+    # Calcula la posición del carácter en el alfabeto.
+    position = ord(char) - ord('a')
+
+    # Calcula la posición del nuevo carácter.
+    new_position = (position + key) % 26
+
+    # Convierte la posición del nuevo carácter a un carácter.
+    new_char = chr(new_position + ord('a'))
+
+    ciphertext += new_char
   return ciphertext
+
 
 def descifrar(ciphertext, key):
   # descifra un mensaje usando el método Cesar y una clave de cifrado
