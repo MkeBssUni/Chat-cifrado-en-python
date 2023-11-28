749ee5cae6de5bb6f5f7b06017692bc1fc1e2a1a
J. Miguel Aguario Diaz <i20213tn095@utez.edu.mx>
2023-11-28 00:27:46 -0600

    [FIX] Cifrar

--- cliente.py
+++ cliente.py
@@ -1,10 +1,10 @@
 import socket
 
-def cifrar_mensaje(mensaje):
+def cifrar_mensaje(mensaje, clave):
     cifrado = ""
     for char in mensaje:
         if char.isalpha():
-            cifrado += chr((ord(char) - 65 + 3) % 26 + 65) if char.isupper() else chr((ord(char) - 97 + 3) % 26 + 97)
+            cifrado += chr((ord(char) - 65 + clave) % 26 + 65) if char.isupper() else chr((ord(char) - 97 + clave) % 26 + 97)
         else:
             cifrado += char
     return cifrado
@@ -20,12 +20,15 @@ def main():
     # Conéctate al servidor
     client.connect((host, port))
 
+    # Solicita la clave al usuario
+    clave = int(input("Ingresa la clave de cifrado (número entero): "))
+
     while True:
         # Lee el mensaje del usuario
         message = input("Yo: ")
 
-        # Cifra el mensaje antes de enviarlo
-        mensaje_cifrado = cifrar_mensaje(message)
+        # Cifra el mensaje antes de enviarlo usando la clave ingresada
+        mensaje_cifrado = cifrar_mensaje(message, clave)
 
         # Envía el mensaje cifrado al servidor
         client.send(mensaje_cifrado.encode('utf-8'))
